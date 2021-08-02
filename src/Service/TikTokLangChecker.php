72715fb7dcb9ca4e20db77860b833e10f93d626f
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2021-08-02 02:53:02 +0200

    check comments

--- src/Service/TikTokLangChecker.php
+++ src/Service/TikTokLangChecker.php
@@ -41,6 +41,22 @@ class TikTokLangChecker
             return LangEnum::FR;
         }
 
+        $fetchedComments = $this->fetcher->query(
+            $this->fetcher->createQuery(
+                'comment'
+            )->select(
+                'content',
+            )->where(
+                'video_id = :video_id'
+            ),
+            ['video_id' => $fetchedVideo['id']]
+        );
+
+        $commentsLang = $this->getLangFromComments($fetchedComments);
+        if ($commentsLang !== null) {
+            return $commentsLang;
+        }
+
         return LangEnum::EN;
     }
 
@@ -63,5 +79,25 @@ class TikTokLangChecker
         }
 
         return false;
+    }
+
+    protected function getLangFromComments(array $comments): ?string
+    {
+        $threshold = count($comments) / 2;
+        $frenchCommentCount = 0;
+
+        foreach ($comments as $comment) {
+            $content = $comment['content'];
+            $isCommentFrench = $this->isFrench($content);
+
+            if ($isCommentFrench) {
+                $frenchCommentCount++;
+            }
+
+            if ($frenchCommentCount >= $threshold) {
+                return LangEnum::FR;
+            }
+        }
+        return null;
     }
 }
